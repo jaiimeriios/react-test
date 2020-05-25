5cb105048834bc4d4e44a48e9d4f84b2a6382b93
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2020-05-25 13:38:35 -0700

    03 Hooks - Pentacore

---
+++ 03 Hooks - Pentacore/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component, useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    
+    const [articulo, setArticulo] = useState([]);
+    const [scrollDepth, setScrollDepth] = useState(0);
+
+    function determineScrollDepth() {
+        const scrolled =
+            document.documentElement.scrollTop || document.body.scrollTop;
+        setScrollDepth(scrolled);
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", determineScrollDepth);
+        return function () {
+            window.removeEventListener("scroll", determineScrollDepth);
+        };
+    });
+
+    return (
+        <div className="App">
+            <h2 className="scroll">Scroll this far: {scrollDepth} px</h2>
+            <hr />
+            <p>Items: {articulo.toString()}</p>
+            <button
+                onClick={() => {
+                    setArticulo([...articulo, "manzana"]);
+                }}
+            >
+                mas 1
+            </button>
+
+            <div className="tall">
+                <img src={logo} className="App-logo" alt="logo" />
+            </div>
+        </div>
+    );
+}
+
+export default App;
