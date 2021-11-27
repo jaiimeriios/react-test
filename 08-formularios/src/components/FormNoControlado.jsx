9d4c6f1d0152d49c5f04be50811a97be15780b4a
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2021-11-26 17:59:31 -0800

    08 react formularios no controlados - Bluuweb

--- 08-formularios/src/components/FormNoControlado.jsx
+++ 08-formularios/src/components/FormNoControlado.jsx
@@ -1,11 +1,52 @@
+import { useRef } from 'react';
+
 function FormNoControlado() {
+    const formulario = useRef(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const datos = new FormData(formulario.current);
+        const objetoDatos = Object.fromEntries([...datos.entries()]);
+
+        // lo mismo desestructurado
+        // if (!objetoDatos.todoName.trim() || !objetoDatos.todoText.trim()) {
+        // console.log(objetoDatos);
+
+        const { todoName, todoText } = objetoDatos;
+        if (!todoName.trim() || !todoText.trim()) {
+            console.log('❌');
+            return;
+        }
+
+        console.log('✅');
+    };
+
     return (
         <>
-            <h2>No Controlado</h2>
-            <form className="no-controlado">
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
+            <form
+                className="no-controlado"
+                ref={formulario}
+                onSubmit={handleSubmit}
+            >
+                <h2>No Controlado</h2>
+                <input
+                    type="text"
+                    name="todoName"
+                    placeholder="Ingrese Tarea"
+                    defaultValue="Tarea"
+                />
+                <textarea
+                    name="todoText"
+                    placeholder="Ingresar descripcion de tarea"
+                    defaultValue="Descripcion Tarea 1"
+                />
+                <select name="todoEstado" defaultValue="pendiente">
+                    <option value="pendiente">Pendiente</option>
+                    <option value="completado">Completado</option>
+                </select>
+
+                <button type="submit">Agregar</button>
             </form>
         </>
     );
