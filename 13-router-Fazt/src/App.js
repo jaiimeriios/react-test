e0c3e017bfe4745c31701ef8b66e81dccc91b6fe
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2021-12-11 11:25:23 -0800

    13 React Router - Fazt
    installing react router dom

---
+++ 13-router-Fazt/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>ROUTER</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
