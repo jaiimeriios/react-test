36d3ccc518bbc077c983f2da7cc664a306b838d7
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2021-12-12 15:29:58 -0800

    14 Router - Bluuweb
    Filter form blog posts

--- 14-router-Bluuweb/src/routes/Blog.jsx
+++ 14-router-Bluuweb/src/routes/Blog.jsx
@@ -1,10 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import { useFetch } from '../hooks/useFetch';
 
 function Blog() {
     const apiUrl = 'https://jsonplaceholder.typicode.com';
 
     const { data, error, loading } = useFetch(`${apiUrl}/posts`);
+
+    let [searchParams, setSearchParams] = useSearchParams();
 
     if (loading) {
         return <h3>Loading...</h3>;
@@ -14,16 +16,35 @@ function Blog() {
         return <h3>{error}</h3>;
     }
 
+    const handleChange = (e) => {
+        let filter = e.target.value;
+        filter ? setSearchParams({ filter }) : setSearchParams({});
+    };
+
     return (
         <>
             <h2>Blog</h2>
-            {data.map((item) => (
-                <h5 key={item.id}>
-                    <Link to={`/blog/${item.id}`}>
-                        {item.id} - {item.title} {item.userId}
-                    </Link>
-                </h5>
-            ))}
+            <input
+                type="text"
+                name=""
+                value={searchParams.get('filter') || ''}
+                onChange={handleChange}
+            />
+            {data
+                .filter((item) => {
+                    let filter = searchParams.get('filter');
+                    if (!filter) return true;
+                    return item.title
+                        .toLowerCase()
+                        .includes(filter.toLowerCase());
+                })
+                .map((item) => (
+                    <h5 key={item.id}>
+                        <Link to={`/blog/${item.id}`}>
+                            {item.id} - {item.title} {item.userId}
+                        </Link>
+                    </h5>
+                ))}
         </>
     );
 }
