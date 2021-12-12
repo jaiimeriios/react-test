4a793702ca7ae96f042cd4be230287d9b710b649
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2021-12-11 22:06:14 -0800

    14 API - Bluuweb
    react router

---
+++ 14-router-Bluuweb/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <header>
+                <img src={logo} className="logo" alt="logo" />
+                <h1>ROUTER</h1>
+            </header>
+        </>
+    );
+}
+
+export default App;
