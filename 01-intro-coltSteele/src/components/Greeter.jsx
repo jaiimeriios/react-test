7f3a9b102d2266e49f4a3a3d590b82b8a73d6653
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2021-12-19 16:02:12 -0800

    01 React Intro - Colt Steele
    Event Listener

--- 01-intro-coltSteele/src/components/Greeter.jsx
+++ 01-intro-coltSteele/src/components/Greeter.jsx
@@ -1,10 +1,27 @@
 function Greeter({ name, age = 0, emocion = 1 }) {
+
+    const handleClick = () => {
+        console.log(name);
+    };
+
     return (
         <div>
             <h2>
                 hola {name} {'!'.repeat(emocion)}
             </h2>
             <h3>Age: {age}</h3>
+
+            <button
+                onClick={handleClick}>
+                    HOLA
+            </button>
+            <button
+                onDoubleClick={() => {
+                    console.log(Number(age));
+                }}
+            >
+                en Linea
+            </button>
         </div>
     );
 }
