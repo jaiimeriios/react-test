86275810134302c38d3fc6cd8949d418b4b20ca9
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2022-01-08 19:31:37 -0800

    react tests
    events

--- test/src/components/EventHandle.jsx
+++ test/src/components/EventHandle.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const EventHandle = () => {
     const [username, setUsername] = useState('');
@@ -6,32 +6,57 @@ const EventHandle = () => {
     const [showPassword, setShowPassword] = useState(false);
 
     return (
-        <div className="container">
-            <input
-                type="text"
-                placeholder="Enter Username"
-                value={username}
-                onChange={(e) => {
-                    setUsername(e.target.value);
-                }}
-            />
-            <input
-                type={showPassword ? 'text' : 'password'}
-                placeholder="Enter Password"
-                value={password}
-                onChange={(e) => {
-                    setPassword(e.target.value);
-                }}
-            />
-            <h2>{username}</h2>
-            <h2>{showPassword ? password : '*'.repeat(password.length)}</h2>
-            <button
-                onClick={(e) => {
-                    setShowPassword(!showPassword);
-                }}
-            >
-                Show/Hide Password
-            </button>
+        <>
+            <Counter />
+
+            <div className="password">
+                <input
+                    type="text"
+                    placeholder="Enter Username"
+                    value={username}
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}
+                />
+                <input
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Enter Password"
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                />
+                <h2>{username}</h2>
+                <h2>{showPassword ? password : '*'.repeat(password.length)}</h2>
+                <button
+                    onClick={(e) => {
+                        setShowPassword(!showPassword);
+                    }}
+                >
+                    Show/Hide Password
+                </button>
+            </div>
+        </>
+    );
+};
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const add = () => {
+        setCount(count + 1);
+    };
+    const sub = () => {
+        setCount(count - 1);
+    };
+
+    return (
+        <div className="counter">
+            <button onClick={sub}>-</button>
+            <span className={count === 0 ? 'zero' : count > 0 ? 'pos' : 'neg'}>
+                {count}
+            </span>
+            <button onClick={add}>+</button>
         </div>
     );
 };
