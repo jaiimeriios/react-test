737290fc50d473b6ca6a8904c07dfc28557f02c4
Jaime Rios <jaimerios1989@gmail.com>
2022-02-02 20:45:16 -0800

    15 Modals - FalconMaster
    Initiating project

---
+++ 16-modal-FalconMaster/src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+function App() {
+    return (
+        <>
+            <Header title="Modal Windows"/>
+            <main>modal</main>
+        </>
+    );
+}
+
+export default App;
