201ab3fc709d8a190544f4fd2559bf85e0376ff3
Jaime Rios <jaimerios1989@gmail.com>
2022-02-02 21:32:11 -0800

    15 Password Generator - FalconMaster
    Modal base styles

--- 16-modal-FalconMaster/src/App.js
+++ 16-modal-FalconMaster/src/App.js
@@ -1,10 +1,20 @@
 import Header from './components/Header';
+import Modal from './components/Modal';
 
 function App() {
     return (
         <>
-            <Header title="Modal Windows"/>
-            <main>modal</main>
+            <Header title="Modal Windows" />
+            <main>
+                <div className="d-flex">
+                    <button className="modal-button">Modal 1</button>
+                    <button className="modal-button">Modal 2</button>
+                    <button className="modal-button">Modal 3</button>
+                    <button className="modal-button">Modal 4</button>
+                </div>
+
+                <Modal />
+            </main>
         </>
     );
 }
