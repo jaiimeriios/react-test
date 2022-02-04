12b5e9e4fffd0af0427b5998faa3229de62d361b
Jaime Rios <jaimerios1989@gmail.com>
2022-02-03 21:47:35 -0800

    17 router 6 - Net Ninja
    starter project

--- 17-router6-NetNinja/src/App.js
+++ 17-router6-NetNinja/src/App.js
@@ -1,36 +1,42 @@
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import logo from './assets/logo.svg';
 
 // pages
-import Home from './pages/Home'
-import About from './pages/About'
-import Products from './pages/Products'
-import ProductDetails from './pages/ProductDetails'
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <nav>
-          <h1>The Ninja Clothing Company</h1>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/products">Products</Link>
-        </nav>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/products/:id">
-            <ProductDetails />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  )
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <nav>
+                    <h1>
+                        <Link to="/">
+                            <img src={logo} />
+                            React Router 6
+                        </Link>
+                    </h1>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/products">Products</Link>
+                </nav>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/products/:id">
+                        <ProductDetails />
+                    </Route>
+                    <Route path="/products">
+                        <Products />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
-export default App
+export default App;
