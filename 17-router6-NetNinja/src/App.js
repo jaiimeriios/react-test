9268097c68b7d9b290f44f8e93c59882a8d0df9d
Jaime Rios <jaimerios1989@gmail.com>
2022-02-03 21:24:19 -0800

    setting up router projects - net ninja & codevolution

--- 17-router6-NetNinja/src/App.js
+++ 17-router6-NetNinja/src/App.js
@@ -1,15 +1,36 @@
-import { useState } from 'react';
-import Header from './components/Header';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
 
 function App() {
-    return (
-        <>
-            <Header title="React Router 6" />
-            <main>
-                router
-            </main>
-        </>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <nav>
+          <h1>The Ninja Clothing Company</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/products/:id">
+            <ProductDetails />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App
