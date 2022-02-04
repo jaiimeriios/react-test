e46f97167c24c49e8a9cefdd28c8a9cd1a0d9cb0
Jaime Rios <jaimerios1989@gmail.com>
2022-02-03 20:07:02 -0800

    15 Password Generator - FalconMaster
    Opening and closing modal

--- 16-modal-FalconMaster/src/App.js
+++ 16-modal-FalconMaster/src/App.js
@@ -1,19 +1,32 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Modal from './components/Modal';
 
 function App() {
+    const [modalEstado1, setEstadoModal1] = useState(false);
+
     return (
         <>
             <Header title="Modal Windows" />
             <main>
                 <div className="d-flex">
-                    <button className="modal-button">Modal 1</button>
+                    <button 
+                        className="modal-button"
+                        onClick={() => setEstadoModal1(!modalEstado1)}
+                    >Modal 1</button>
                     <button className="modal-button">Modal 2</button>
                     <button className="modal-button">Modal 3</button>
                     <button className="modal-button">Modal 4</button>
                 </div>
 
-                <Modal />
+                <Modal
+                    estado={modalEstado1}
+                    setEstado={setEstadoModal1}
+                >
+                    <h3>hola</h3>
+                    <p>Lorem ipsum dolor sit amet </p>
+                    <button>Aceptar</button>
+                </Modal>
             </main>
         </>
     );
