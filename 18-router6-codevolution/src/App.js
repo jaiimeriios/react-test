888884c23943924ea36ee5f2d29f12876f2afdc6
Jaime Rios <jaimerios1989@gmail.com>
2022-02-05 18:30:29 -0800

    18 React Router 6 - Codevolution
    CLEANUP

--- 18-router6-codevolution/src/App.js
+++ 18-router6-codevolution/src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import Nav from './components/Nav';
 
 function App() {
     return (
         <>
             <Header title="React Router 6" />
+            <Nav />
             <main>router</main>
         </>
     );
