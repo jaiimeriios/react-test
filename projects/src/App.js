70b265427872906c309b53a9066ca3f637d3ab55
Jaime Rios <jaimerios1989@gmail.com>
2022-02-08 18:01:14 -0800

    started template - react projects

---
+++ projects/src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+function App() {
+    return (
+        <>
+            <Header title="React Projects" />
+            <main>Main</main>
+        </>
+    );
+}
+
+export default App;
---
+++ projects/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { PropTypes } from 'prop-types';
+import Nav from './Nav';
+import logo from '../assets/logo.svg';
+
+function Header({ title }) {
+    return (
+        <>
+            <header>
+                <img src={logo} className="logo" alt="logo" />
+                <h1>{title}</h1>
+            </header>
+            <Nav />
+        </>
+    );
+}
+
+Header.defaultProps = {
+    title: 'TITLE',
+};
+
+Header.protoTypes = {
+    title: PropTypes.string.isRequired,
+};
+
+export default Header;
