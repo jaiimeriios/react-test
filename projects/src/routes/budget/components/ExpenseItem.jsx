a42ea02369eb21e462be8065b63f94c02b6067ff
Jaime Rios <jaimerios1989@gmail.com>
2022-02-19 15:43:25 -0800

    Budget planner - delete items from list

--- projects/src/routes/budget/components/ExpenseItem.jsx
+++ projects/src/routes/budget/components/ExpenseItem.jsx
@@ -1,11 +1,23 @@
 import { TiDelete } from 'react-icons/ti';
 
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
 const ExpenseItem = ({ id, name, cost }) => {
+    const { dispatch } = useContext(AppContext);
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: id,
+        });
+    };
+
     return (
         <li>
             {name}
             <span>${cost}</span>
-            <TiDelete size="1.5rem"></TiDelete>
+            <TiDelete size="1.5rem" onClick={handleDeleteExpense}></TiDelete>
         </li>
     );
 };
--- projects/src/routes/budget/context/AppContext.js
+++ projects/src/routes/budget/context/AppContext.js
@@ -4,9 +4,17 @@ const AppReducer = (state, action) => {
     switch (action.type) {
         case 'ADD_EXPENSE':
             return {
-                ...state,                                        // copy of current state
-                expenses: [...state.expenses, action.payload],   // overwrite current with copy
-            };                                                   // of current and what's added 
+                ...state,                                       // copy of current state
+                expenses: [...state.expenses, action.payload],  // overwrite current with copy
+            };                                                  // of current and what's added
+
+        case 'DELETE_EXPENSE':
+            return {
+                ...state,                                       // copy of current state
+                expenses: state.expenses.filter(                // return new array without the 
+                    (expense) => expense.id != action.payload   // selected item
+                ),
+            };
         default:
             return state;
     }
